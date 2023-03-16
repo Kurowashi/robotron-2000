2e9a48ad7709742be89bfeb95a2f16c1f3c5f18f
Luan Rocha <kuro.winchester@gmail.com>
2023-03-15 22:11:03 -0300

    Projeto finalizado

--- main.js
+++ main.js
@@ -1,23 +1,62 @@
-const braco = document.querySelector("#braco")
-const add = document.querySelector("#add")
-const sub = document.querySelector("#sub")
+const controle = document.querySelectorAll("[data-controle]")
+const estatisticas = document.querySelectorAll("[data-estatistica]")
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
 
-const controle = document.querySelectorAll(".controle-ajuste")
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos": {
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas": {
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes": {
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 
 controle.forEach((elemento) => {
     elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.textContent)
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+        atualizaEstatistica(evento.target.dataset.peca)
     })
 })
 
-function manipulaDados(operador) {
+function manipulaDados(operador, controle) {
+    const peca = controle.querySelector("[data-contador]")
     if (operador === "-") {
-        if (parseInt(braco.value) > 0) {
-            braco.value = parseInt(braco.value) - 1
+        if (parseInt(peca.value) > 0) {
+            peca.value = parseInt(peca.value) - 1
         } else {
             return
         }
     } else {
-        braco.value = parseInt(braco.value) + 1
+        peca.value = parseInt(peca.value) + 1
     }
+}
+
+function atualizaEstatistica(peca) {
+    estatisticas.forEach((elemento) => {
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+        console.log(pecas[peca][elemento.dataset.estatistica])
+    })
 }
